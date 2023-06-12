2ff371dbfb03bda663aa32c989d772e1f370be56
naramos00 <125921189+Naramos00@users.noreply.github.com>
2023-06-11 21:38:40 -0700

    double clicking now toggles continuous stroke

--- etch.js
+++ etch.js
@@ -20,3 +20,45 @@ function createGrid(userInput){
 let grid = Number(prompt('How many units per side?'));
 
 createGrid(grid);
+const hoverEl = document.querySelectorAll('.column');
+const containerClick = document.getElementById('grid-container');
+const body = document.querySelector('body');
+let count = 0;
+
+
+
+containerClick.onclick = singleBlock();
+body.ondblclick = dblClick;
+//need an explanation for why dblClick() fires dblclick event, but dblClick with out () doesnt
+
+function singleBlock(){
+
+    hoverEl.forEach((e) => e.addEventListener('mousedown', () => {
+        e.style.background = 'black';
+    }));
+}
+
+function dblClick(){
+    count += 1;
+    if(count % 2 != 0){
+        console.log(count);
+        toggleSmooth();
+    }
+    else{
+        console.log(count);
+        toggleSmoothOff();
+    }
+}
+
+function toggleSmooth(){
+    hoverEl.forEach((e) => e.addEventListener('mouseover', handleSmooth));
+}
+
+function toggleSmoothOff(){
+    hoverEl.forEach((e) => e.removeEventListener('mouseover', handleSmooth));
+}
+
+function handleSmooth(e){
+    e.target.style.background = 'black';
+}
+
