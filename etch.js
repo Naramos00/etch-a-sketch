8382714fd60e787eba39225bd382e14f5c222929
naramos00 <125921189+Naramos00@users.noreply.github.com>
2023-06-15 02:24:06 -0700

    add clear and rainbow pen buttons

--- etch.js
+++ etch.js
@@ -24,6 +24,9 @@ const hoverEl = document.querySelectorAll('.column');
 const containerClick = document.getElementById('grid-container');
 const body = document.querySelector('body');
 let count = 0;
+let rainbowBtnOn = false;
+
+
 
 
 
@@ -32,7 +35,6 @@ body.ondblclick = dblClick;
 //need an explanation for why dblClick() fires dblclick event, but dblClick with out () doesnt
 
 function singleBlock(){
-
     hoverEl.forEach((e) => e.addEventListener('mousedown', () => {
         e.style.background = 'black';
     }));
@@ -59,6 +61,70 @@ function toggleSmoothOff(){
 }
 
 function handleSmooth(e){
-    e.target.style.background = 'black';
+    if(rainbowBtnOn == true){
+        e.target.style.background = `${generateRandom()}`;
+    }
+    else{
+        e.target.style.background = 'black';
+    }
+}
+
+function clear(){
+    const allColumns = document.querySelectorAll('.column');
+    allColumns.forEach((e) => e.style.background = 'transparent');
+}
+const clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', clear);
+
+const eraseBtn = document.getElementById('erase');
+eraseBtn.onclick = toggleErase;
+let eraseBtnOn = false;
+
+function toggleErase(){
+    if(!eraseBtnOn){
+        eraseBtnOn = true;
+        hoverEl.forEach((e) => e.addEventListener('mouseover', eraseOn));
+        eraseBtn.classList.add("eraseon");
+    }
+    else{
+        eraseBtnOn = false;
+        hoverEl.forEach((e) => e.removeEventListener('mouseover', eraseOn));
+        eraseBtn.classList.remove('eraseon');
+
+
+    }
+}
+
+function eraseOn(e){
+    e.target.style.background = 'transparent';
+}
+
+function generateRandom(){
+    let maxVal = 0xFFFFFF; // 16777215
+    let randomNumber = Math.random() * maxVal; 
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0); 
+    
+    return `#${randColor.toUpperCase()}`
+}
+
+const rainbowBtn = document.getElementById('rainbow');
+rainbowBtn.onclick = toggleRainbow;
+
+function toggleRainbow(){
+    console.log(rainbowBtnOn);
+    if(!rainbowBtnOn){
+        rainbowBtnOn = true;
+        hoverEl.forEach((e) => e.addEventListener('click', RainbowOn))
+    }
+    else{
+        rainbowBtnOn = false;
+        hoverEl.forEach((e) => e.removeEventListener('click', RainbowOn));
+    }
+}
+
+function RainbowOn(e){
+    e.target.style.background = `${generateRandom()}`;
 }
 
